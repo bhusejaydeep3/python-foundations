69a71e00d22e51d6c03234a6fd7ab946340cb46c
jaydeep_bhuse <bhusejaydeep3@gmail.com>
2026-02-09 11:43:18 +0530

    simplified  explaination

--- 01_basics/variables.py
+++ 01_basics/variables.py
@@ -1,3 +1,15 @@
-name = "Jaydeep"
-age = 19
-print(name, age)
+''' 
+Variables example:
+
+Variables are used to store data in memory 
+so it can be reused later in the program.
+'''
+name ="jaydeep"
+age = 19 
+height = 5.9 
+is_student = True 
+
+print("name:", name)
+print("age:", age)
+print("Height:", height)
+print("stduent:", is_student)
